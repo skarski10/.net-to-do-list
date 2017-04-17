0084efa9e5c504c8c53f3173248355d550208b25
Brandon Cooper, Kory Skarbek and Sandro Mijatovic <brandon+kory+sandro@>
2017-04-17 15:08:11 -0700

    add project

---
+++ src/ToDoList/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ToDoList.Models
+{
+    [Table("Items")]
+    public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+    }
+}
